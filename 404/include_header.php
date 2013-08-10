5ddbf3fcb5136e72833a395b45ff367100cc68a0
Xavi Rigau <xrigau@gmail.com>
2013-08-10 19:14:48 +0100

    Created 404 folder that will be displayed on errors

---
+++ 404/include_header.php
@@ -0,0 +1,24 @@
+<div id="top_header">
+	<header class="clearfix">
+		<a id="title_text" href="index.php">xavirigau.com</a>
+		<nav>
+			<ul>
+				<li id="home">
+					<a href="index.php" class="nav_options">About me</a>
+				</li>
+				<li id="resume">
+					<a href="resume.php" class="nav_options">Resume</a>
+				</li>
+				<li id="portfolio">
+					<a href="portfolio.php" class="nav_options">Portfolio</a>
+				</li>
+				<li id="blog">
+					<a href="http://blog.xavirigau.com" class="nav_options">Blog</a>
+				</li>
+				<li id="github">
+					<a href="https://github.com/xrigau" class="nav_options">GitHub</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</div>
